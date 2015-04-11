3925803387b693794ded9bb14b140e4a017f13b0
Gert Van Gool <gertvangool@gmail.com>
2015-04-11 15:13:18 +0200

    Put the version number directly in the files

--- django_states/__init__.py
+++ django_states/__init__.py
@@ -6,8 +6,5 @@ Define a state graph for a model and remember the state of each object.
 State transitions can be logged for objects.
 """
 
-#: The version list
-VERSION = (1, 6, 4)
-
-#: The actual version number, used by python (and shown in sentry)
-__version__ = '.'.join(map(str, VERSION))
+#: The version number (used by python and shown in sentry)
+__version__ = "1.6.4"
